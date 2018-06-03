e996f1d1f91a1ca4eb2f10ed0e7287bf6b929244
Xpitfire <dinu.marius-constantin@hotmail.com>
2018-06-03 17:59:22 +0200

    Generalized expression

--- mac_copy-content.sh
+++ mac_copy-content.sh
@@ -1,3 +1,4 @@
-cp -R /Applications/Apollon.app/Contents/Frameworks ./Apollon-Qt.app/Contents/Frameworks
-cp -R /Applications/Apollon.app/Contents/Plugins ./Apollon-Qt.app/Contents/Plugins
-cp /Applications/Apollon.app/Contents/Resources/qt.conf ./Apollon-Qt.app/Contents/Resources/qt.conf
+WORKING_APOLLON_BUILD_APP_DIR=/Applications/Apollon.app
+cp -R $(WORKING_APOLLON_BUILD_APP_DIR)/Contents/Frameworks ./Apollon-Qt.app/Contents/Frameworks
+cp -R $(WORKING_APOLLON_BUILD_APP_DIR)/Contents/Plugins ./Apollon-Qt.app/Contents/Plugins
+cp $(WORKING_APOLLON_BUILD_APP_DIR)/Contents/Resources/qt.conf ./Apollon-Qt.app/Contents/Resources/qt.conf
