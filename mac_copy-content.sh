6a62e00eb82d7408f4e932a45418705f3739de46
Xpitfire <dinu.marius-constantin@hotmail.com>
2018-06-03 17:54:12 +0200

    Fixed wrong c++ path and added mac copy framework references

---
+++ mac_copy-content.sh
@@ -0,0 +1,3 @@
+cp -R /Applications/Apollon.app/Contents/Frameworks ./Apollon-Qt.app/Contents/Frameworks
+cp -R /Applications/Apollon.app/Contents/Plugins ./Apollon-Qt.app/Contents/Plugins
+cp /Applications/Apollon.app/Contents/Resources/qt.conf ./Apollon-Qt.app/Contents/Resources/qt.conf
